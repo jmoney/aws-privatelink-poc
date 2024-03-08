03136989847aa2e48efec6ce66eb541c5f8fe284
J$ <jon.monette@gmail.com>
2024-03-08 11:40:51 -0600

    update to remove TODOs and update public lb healthcheck

--- main.tf
+++ main.tf
@@ -11,28 +11,31 @@ provider "aws" {
     region = "us-east-1"
 }
 
+locals {
+    availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
 module "provider_network" {
     source = "./modules/network_segment"
     cidr_block = "10.0.0.0/16"
-    availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    availability_zones = local.availability_zones
 }
 
 module "consumer_network" {
     source = "./modules/network_segment"
     cidr_block = "10.1.0.0/16"
-    availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    availability_zones = local.availability_zones
 }
 
 module "echo_server" {
     source = "./modules/echo_server"
-    subnet_id = module.provider_network.subnet_ids["private"]["us-east-1a"]
+    subnet_id = module.provider_network.subnet_ids["private"][element(local.availability_zones, 0)]
     vpc_id = module.provider_network.vpc_id
 }
 
 module "provider" {
     source = "./modules/provider"
-    # TODO - grab all private subnets from the provider network
-    subnet_ids = [module.provider_network.subnet_ids["private"]["us-east-1a"], module.provider_network.subnet_ids["private"]["us-east-1b"]]
+    subnet_ids = [for az, subnet in module.consumer_network.subnet_ids["private"] : subnet]
     instance_ip = module.echo_server.instance_ip
     cidr_block = var.provider_ingress_cidr_block
 }
@@ -55,8 +58,7 @@ resource "aws_lb" "public_lb" {
     name = "public-lb"
     internal = false
     load_balancer_type = "application"
-    # TODO - grab all public subnets from the consumer network
-    subnets = [module.consumer_network.subnet_ids["public"]["us-east-1a"], module.consumer_network.subnet_ids["public"]["us-east-1b"]]
+    subnets = [for az, subnet in module.consumer_network.subnet_ids["public"] : subnet]
     enable_deletion_protection = false
     tags = {
         Name = "public-lb"
@@ -81,9 +83,9 @@ resource "aws_lb_target_group" "public_lb" {
     vpc_id = module.consumer_network.vpc_id
 
     health_check {
-        path = "/health"
+        path = "/http"
         protocol = "HTTP"
-        port = "traffic-port"
+        port = "9001"
         interval = 30
         timeout = 5
         healthy_threshold = 2
@@ -92,8 +94,7 @@ resource "aws_lb_target_group" "public_lb" {
 }
 
 resource "aws_lb_target_group_attachment" "consumers" {
-    # TODO - map this to the consumer network availability zones
-    count = 3
+    count = length(local.availability_zones)
     target_group_arn = aws_lb_target_group.public_lb.arn
     target_id = module.consumer.consumer_ips[count.index]
     port = 80
